f5ea259f43f154efc5641a9418a40e30b553a11b
Marcin Gromek <gromson101@gmail.com>
2021-02-15 13:06:30 +0100

    add code formatting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,30 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   env: {
     node: true,
   },
   rules: {
-    'object-shorthand': ['error', 'always', {
-      avoidQuotes: true,
-      avoidExplicitReturnArrows: true,
-    }],
-    'function-paren-newline': ['error', 'consistent'],
-    'max-len': ['warn', 120, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
+    "object-shorthand": [
+      "error",
+      "always",
+      {
+        avoidQuotes: true,
+        avoidExplicitReturnArrows: true,
+      },
+    ],
+    quotes: [2, "single"],
+    "function-paren-newline": ["error", "consistent"],
+    "max-len": [
+      "warn",
+      120,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
 };
